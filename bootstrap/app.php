6181f01e424bb654bedfdbeb435ae7b5bddbd588
IIPN Solutions <guptanishantmca@gmail.com>
2024-12-12 21:45:14 +0530

    added roles middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \App\Http\Middleware\SetLocale::class,
+            \App\Http\Middleware\RoleMiddleware::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
