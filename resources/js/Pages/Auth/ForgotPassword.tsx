c124bc3077ecfdbc5a44ff3f9f8e134b0c28cf85
IIPN Solutions <guptanishantmca@gmail.com>
2025-02-09 21:32:17 +0530

    updated

--- resources/js/Pages/Auth/ForgotPassword.tsx
+++ resources/js/Pages/Auth/ForgotPassword.tsx
@@ -24,7 +24,7 @@ export default function ForgotPassword({ status }: { status?: string }) {
                 Forgot your password? No problem. Just let us know your email
                 address and we will email you a password reset link that will
                 allow you to choose a new one.
-            </div>
+            </div> 
 
             {status && (
                 <div className="mb-4 text-sm font-medium text-green-600">
